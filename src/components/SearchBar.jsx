6d0745b22b8b215f1a849b898e533f0bfcb47f90
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2020-11-16 17:37:54 -0800

    adds onSubmit function in App and onFormSubmit in SearchBar.js

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    state = { term: '' }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
-        return <div>
-            <div className='ui segment'>
-                <form className='ui form'>
-                    <div className='field'>
-                    <label>Image Search</label>
-                    <input type='text' />
-                    </div>
-                </form>
+        return (
+            <div>
+                <div className='ui segment'>
+                    <form onSubmit={this.onFormSubmit} className='ui form'>
+                        <div className='field'>
+                            <label>Image Search</label>
+                            <input
+                                type='text'
+                                value={this.state.term}
+                                onChange={(e) => this.setState({ term: e.target.value })}
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        )
     }
 }
 
