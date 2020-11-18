109f743e84ee7c60815bd566d684105211f3a66b
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2020-11-17 22:40:51 -0800

    adds unsplash api component, imageCard component, and refactors app.js and ImageList to render images correctly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import axios from 'axios'
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
+import ImageList from './ImageList'
 
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term},
-            headers: {
-                Authorization: 'Client-ID ${process.env.CLIENT_ID}'
-            }
+    state = { images: [] }
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },
         })
+        this.setState({ images: response.data.results })
     }
-    
+
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         )
     }
